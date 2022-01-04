0ff737cd179a5664da0a64cfc68d792007a5c688
josua-reyes <72959390+josua-reyes@users.noreply.github.com>
2022-01-03 19:07:35 -0600

    Funcionalidad inicial

---
+++ DateTimePicker/DateTimePicker/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿
+namespace DateTimePicker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dtpFecha1 = new System.Windows.Forms.DateTimePicker();
+            this.dtpFecha2 = new System.Windows.Forms.DateTimePicker();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtResultado = new System.Windows.Forms.TextBox();
+            this.btnCalcularDias = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Fecha 1:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(23, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Fecha 2:";
+            // 
+            // dtpFecha1
+            // 
+            this.dtpFecha1.CustomFormat = "MM/dd/yyyy hh:mm:ss";
+            this.dtpFecha1.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dtpFecha1.Location = new System.Drawing.Point(80, 22);
+            this.dtpFecha1.Name = "dtpFecha1";
+            this.dtpFecha1.Size = new System.Drawing.Size(460, 20);
+            this.dtpFecha1.TabIndex = 2;
+            this.dtpFecha1.Value = new System.DateTime(2022, 1, 3, 18, 6, 17, 0);
+            // 
+            // dtpFecha2
+            // 
+            this.dtpFecha2.CustomFormat = "MM/dd/yyyy hh:mm:ss";
+            this.dtpFecha2.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dtpFecha2.Location = new System.Drawing.Point(80, 60);
+            this.dtpFecha2.Name = "dtpFecha2";
+            this.dtpFecha2.Size = new System.Drawing.Size(460, 20);
+            this.dtpFecha2.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(23, 107);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Resultado:";
+            // 
+            // txtResultado
+            // 
+            this.txtResultado.Location = new System.Drawing.Point(80, 100);
+            this.txtResultado.Name = "txtResultado";
+            this.txtResultado.Size = new System.Drawing.Size(460, 20);
+            this.txtResultado.TabIndex = 5;
+            // 
+            // btnCalcularDias
+            // 
+            this.btnCalcularDias.Location = new System.Drawing.Point(80, 136);
+            this.btnCalcularDias.Name = "btnCalcularDias";
+            this.btnCalcularDias.Size = new System.Drawing.Size(460, 23);
+            this.btnCalcularDias.TabIndex = 6;
+            this.btnCalcularDias.Text = "&Calcular Tiempo Transcurrido";
+            this.btnCalcularDias.UseVisualStyleBackColor = true;
+            this.btnCalcularDias.Click += new System.EventHandler(this.btnCalcularDias_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(579, 187);
+            this.Controls.Add(this.btnCalcularDias);
+            this.Controls.Add(this.txtResultado);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.dtpFecha2);
+            this.Controls.Add(this.dtpFecha1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "DateTimePicker";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DateTimePicker dtpFecha1;
+        private System.Windows.Forms.DateTimePicker dtpFecha2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtResultado;
+        private System.Windows.Forms.Button btnCalcularDias;
+    }
+}
+
---
+++ DateTimePicker/DateTimePicker/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DateTimePicker
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcularDias_Click(object sender, EventArgs e)
+        {
+            DateTime dtFecha1;
+            DateTime dtFecha2;
+            TimeSpan tsTiempoTranscurrido;
+            DateTime dtiempoTranscurrido;
+            string strAnios;
+            string strMeses;
+            string strDias;
+            string strHoras;
+            string strMinutos;
+            string strSegundos;
+            string StrResultado;
+
+            dtFecha1 = dtpFecha1.Value;
+            dtFecha2 = dtpFecha2.Value;
+            tsTiempoTranscurrido = dtFecha2 - dtFecha1;
+            dtiempoTranscurrido = new DateTime(tsTiempoTranscurrido.Ticks);
+            strAnios = (dtiempoTranscurrido.Year - 1).ToString();
+            strMeses = (dtiempoTranscurrido.Month - 1).ToString();
+            strDias = (dtiempoTranscurrido.Day - 1).ToString();
+            strHoras = dtiempoTranscurrido.Hour.ToString();
+            strMinutos = dtiempoTranscurrido.Minute.ToString();
+            strSegundos = dtiempoTranscurrido.Second.ToString();
+
+            StrResultado = String.Format("{0} Años, {1} Meses, {2} Días, {3} Horas, {4} Minutos, {5} Segundos",
+               strAnios, strMeses, strDias, strHoras, strMinutos, strSegundos);
+
+            txtResultado.Text = StrResultado;
+        }
+    }
+}
